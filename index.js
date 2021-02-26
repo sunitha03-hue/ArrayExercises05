92720eafab23e5751f7707c594cecf3542ca2304
Sunitha Iyadurai <sunithaiyadurai@gmail.com>
2021-02-26 01:44:32 +0000

    Java Exercise 8.5 Part 5 Completed

--- index.js
+++ index.js
@@ -2,16 +2,33 @@ let str = 'In space, no one can hear you code.';
 let arr = ['B', 'n', 'n', 5];
 
 //a) Use the split method on the string to identify the purpose of the parameter inside the ().
+//str=str.split();
+console.log("***SPLIT***")
+console.log(str.split());
 
+console.log(str.split('e'));
 
+console.log(str.split(' '));
+
+console.log(str.split(''));
 
 //b) Use the join method on the array to identify the purpose of the parameter inside the ().
+console.log("***JOIN***");
+console.log(arr.join());
 
+console.log(arr.join('a'));
 
+console.log(arr.join(' '));
 
+console.log(arr.join(''));
 //c) Do split or join change the original string/array?
 
-
+console.log(str);
+console.log(arr);
 
 //d) We can take a comma-separated string and convert it into a modifiable array. Try it! Alphabetize the cargoHold string, and then combine the contents into a new string.
+console.log("***SPLIT&JOIN***")
 let cargoHold = "water,space suits,food,plasma sword,batteries";
+
+console.log(cargoHold.split(',').sort().join(','));
+
